465126e2b113053f5f61a7c7e971efa578c3b673
lmfmay <176079985+lmfmay@users.noreply.github.com>
2024-09-17 21:45:28 -0700

    Solved Part 1

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+//PART 1 GETTING STARTED
+    // Select and cache the <main> element in a variable named mainEl.
+    let mainEl = document.querySelector(`main`);
+    console.log(mainEl);
+    // Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
+    // Hint: Assign a string that uses the CSS var() function like this: 'var(--main-bg)'.
+    mainEl.style.backgroundColor = 'var(--main-bg)';
+    // Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; pick whichever one that you think works best in this situation.
+    mainEl.innerHTML = `<h1>DOM MANIPULATION</h1>`;
+    // Add a class of flex-ctr to mainEl.
+    // Hint: Use the Element.classList API.
+    mainEl.classList.add(`flex-ctr`);
+
+//PART 2 CREATING A MENU BAR
+// Next, create a blank menu bar that we can use to later add some interactivity to the page:
+// Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+let topMenuEl = document.querySelector(`#top-menu`);
+// Set the height of the topMenuEl element to be 100%.
+
+// Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+// Add a class of flex-around to topMenuEl.
